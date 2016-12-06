0e0073a069513d7b1522b5ee8b3a63233d59f9e2
ericwen229 <eericwen@icloud.com>
2016-12-06 15:28:54 +0800

    Setup Spring Boot frameword

---
+++ src/main/java/com/ericwen229/webdict/WebdictApplication.java
@@ -0,0 +1,12 @@
+package com.ericwen229.webdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdictApplication.class, args);
+	}
+}
