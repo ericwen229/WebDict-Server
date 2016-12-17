0e6619caa448a15aa5273d96f9fdf99897e5d2a5
ericwen229 <eericwen@icloud.com>
2016-12-17 13:19:50 +0800

    Implement all three queries

--- src/main/java/com/ericwen229/webdict/model/Query.java
+++ src/main/java/com/ericwen229/webdict/model/Query.java
@@ -1,10 +1,14 @@
 package com.ericwen229.webdict.model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -117,6 +121,7 @@ public class Query {
                 if (transMatcher.find()) translation = transMatcher.group(1);
                 translation = translation.replace("\"", "");
                 translation = translation.replace(",", ";");
+                translation = translation.replace("\\u2026", "...");
             }
         }
         catch (Exception e) {
@@ -138,9 +143,64 @@ public class Query {
     }
 
     public void queryJinshan() {
-        explanations.add(new Explanation("jinshan", "success",
-                "en jinshan " + word, "us jinshan " + word,
-                "tr jinshan " + word, (int)(Math.random() * 100)));
+        if (word.length() == 0) {
+            return;
+        }
+        StringBuffer result = new StringBuffer();
+        BufferedReader in = null;
+        String status = "", enPhonetic = "", usPhonetic = "";
+        StringBuffer translation = new StringBuffer();
+        try {
+            String url = "http://dict-co.iciba.com/api/dictionary.php?key=FD186122ADD1932EDD67B5E84AF2D90A&type=json&w=" + word;
+            URL realUrl = new URL(url);
+            URLConnection conn = realUrl.openConnection();
+            conn.connect();
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+            String resultStr = result.toString();
+            JSONObject resultJson = new JSONObject(resultStr);
+            resultJson = resultJson.getJSONArray("symbols").getJSONObject(0);
+            enPhonetic = resultJson.getString("ph_en");
+            usPhonetic = resultJson.getString("ph_am");
+            JSONArray translationArray = resultJson.getJSONArray("parts");
+            for (int i = 0; i < translationArray.length(); ++ i) {
+                JSONObject part = translationArray.getJSONObject(i);
+                if (i != 0) {
+                    translation.append(";");
+                }
+                translation.append(part.getString("part"));
+                translation.append(' ');
+                JSONArray means = part.getJSONArray("means");
+                for (int j = 0; j < means.length(); ++ j) {
+                    if (j != 0) {
+                        translation.append("，");
+                    }
+                    translation.append(means.getString(j));
+                }
+            }
+            status = "success";
+        }
+        catch (Exception e) {
+            status = "fail";
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            }
+            catch (Exception e2) {
+                status = "fail";
+                e2.printStackTrace();
+            }
+        }
+        explanations.add(new Explanation("jinshan", status,
+                enPhonetic, usPhonetic,
+                translation.toString(), (int)(Math.random() * 100)));
     }
 
     public String getWord() {
