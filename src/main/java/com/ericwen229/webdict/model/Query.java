06ac11c36e295acfe000a302a5a7561e4b8746eb
ericwen229 <eericwen@icloud.com>
2016-12-18 12:01:59 +0800

    Small fixes

--- src/main/java/com/ericwen229/webdict/model/Query.java
+++ src/main/java/com/ericwen229/webdict/model/Query.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
