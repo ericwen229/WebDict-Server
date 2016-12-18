5fa154d93210a666a73143fa1e626250318d1fe2
ericwen229 <eericwen@icloud.com>
2016-12-18 10:03:24 +0800

    Add database operations to like

--- src/main/java/com/ericwen229/webdict/controller/LikeController.java
+++ src/main/java/com/ericwen229/webdict/controller/LikeController.java
@@ -6,14 +6,69 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
+
 @RestController
 public class LikeController {
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/webdict";
+
+    static final String USER = "eric";
+    static final String PWD = "professional*";
 
     @RequestMapping(value = "/like", method = RequestMethod.GET)
     public Like like(@RequestParam(value = "word") String word,
                      @RequestParam(value = "source") String source,
                      @RequestParam(value = "dislike", defaultValue = "false") String dislike) {
-        return new Like(true);
+        Connection conn = null;
+        Statement stmt = null;
+        boolean success = true;
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PWD);
+            stmt = conn.createStatement();
+            word = word.replace(" ", "");
+            ResultSet rs = stmt.executeQuery(String.format("SELECT * FROM likes WHERE word='%s' LIMIT 1", word));
+            if (rs.next()) {
+                int num = rs.getInt(source);
+                if (dislike.equals("true")) {
+                    ++ num;
+                }
+                else {
+                    -- num;
+                }
+                stmt.executeUpdate(String.format("UPDATE likes SET %s=%d WHERE word='%s'", source, num + 1, word));
+            }
+            else {
+                stmt.executeUpdate(String.format("INSERT INTO likes VALUES ('%s', 0, 0, 0)", word));
+                stmt.executeUpdate(String.format("UPDATE likes SET %s=%d WHERE word='%s'", source, 1, word));
+            }
+        }
+        catch (Exception e) {
+            success = false;
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (stmt != null) {
+                    conn.close();
+                }
+            }
+            catch (SQLException se) {
+                success = false;
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+            catch (SQLException se) {
+                success = false;
+                se.printStackTrace();
+            }
+        }
+        return new Like(success);
     }
 
 }
